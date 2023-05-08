a3cf71de4ac44466276bcfe70a3157b6a4b9b611
hoangnh280 <hoangnh280@git.ghtk.vn>
2023-05-08 11:44:45 +0700

    fix(lib)

--- src/main/java/org/delta/Delta.java
+++ src/main/java/org/delta/Delta.java
@@ -5,24 +5,44 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.streaming.StreamingQueryException;
+import org.apache.spark.sql.types.StructType;
+
+import java.util.concurrent.TimeoutException;
 
 public class Delta {
-    public static void main(String[] args) {
-        // Create a SparkConf object to configure the Spark application
-        SparkConf conf = new SparkConf().setAppName("Word Count").setMaster("local[*]");
+  public static void main(String[] args) throws TimeoutException, StreamingQueryException {
+    // Create a SparkConf object to configure the Spark application
+    SparkConf conf = new SparkConf().setAppName("DeltaApp").setMaster("local[*]");
 
-        // Create a JavaSparkContext object to connect to the Spark cluster
-        JavaSparkContext sc = new JavaSparkContext(conf);
+    // Create a JavaSparkContext object to connect to the Spark cluster
+    JavaSparkContext sc = new JavaSparkContext(conf);
 
-        // spark session
-        SparkSession spark = SparkSession.builder()
-                .appName("Java Spark SQL basic example")
-                .getOrCreate();
+    // spark session
+    SparkSession spark = SparkSession.builder()
+        .appName("DeltaAppExample")
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+        .getOrCreate();
 
-        Dataset<Row> data = spark.range(0, 5).toDF();
-        data.write().format("delta").save("/tmp/delta-table");
+    // create a schema for the delta table
+    StructType schema = new StructType()
+        .add("id", "integer")
+        .add("name", "string")
+        .add("age", "integer");
 
-        // Stop the Spark context
-        sc.stop();
-    }
+    // read data from a csv file
+    Dataset<Row> df = spark.readStream().schema(schema).csv("data.csv");
+
+    // df.write().format("delta").save("delta-table");
+    df.writeStream().format("console").outputMode("append").start().awaitTermination();
+
+    // read the delta table
+    // Dataset<Row> df2 = spark.read().format("delta").load("delta-table");
+    //
+    // df2.show();
+
+    // Stop the Spark context
+    sc.stop();
+  }
 }
--- src/main/java/org/delta/Main.java
+++ src/main/java/org/delta/Main.java
@@ -9,31 +9,32 @@ import scala.Tuple2;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        // Create a SparkConf object to configure the Spark application
-        SparkConf conf = new SparkConf().setAppName("Word Count").setMaster("local[*]");
+  public static void main(String[] args) {
+    // Create a SparkConf object to configure the Spark application
+    SparkConf conf = new SparkConf().setAppName("Word Count").setMaster("local[*]");
 
-        // Create a JavaSparkContext object to connect to the Spark cluster
-        JavaSparkContext sc = new JavaSparkContext(conf);
+    // Create a JavaSparkContext object to connect to the Spark cluster
+    JavaSparkContext sc = new JavaSparkContext(conf);
 
-        // Load the text file into an RDD
-        JavaRDD<String> lines = sc.textFile("input.txt");
+    // Load the text file into an RDD
+    JavaRDD<String> lines = sc.textFile("input.txt");
 
-        // Split each line into words and create a new RDD of words
-        JavaRDD<String> words = lines.flatMap(line -> Arrays.asList(line.split(" ")).iterator());
+    // Split each line into words and create a new RDD of words
+    JavaRDD<String> words = lines.flatMap(line -> Arrays.asList(line.split(" ")).iterator());
 
-        // Map each word to a key-value pair of (word, 1) and reduce by key to count the occurrences of each word
-        JavaPairRDD<String, Integer> wordCounts = words.mapToPair(word -> new Tuple2<>(word, 1))
-                .reduceByKey((count1, count2) -> count1 + count2);
+    // Map each word to a key-value pair of (word, 1) and reduce by key to count the occurrences of each word
+    JavaPairRDD<String, Integer> wordCounts = words
+        .mapToPair(word -> new Tuple2<>(word, 1))
+        .reduceByKey(Integer::sum);
 
-        // Print the word counts
-        wordCounts.foreach(pair -> {
-            if (pair._2() > 20) {
-                System.out.println(pair._1() + ": " + pair._2());
-            }
-        });
+    // Print the word counts
+    wordCounts.foreach(pair -> {
+      if (pair._1.toLowerCase().contains("pattern")) {
+        System.out.println(pair._1() + ": " + pair._2());
+      }
+    });
 
-        // Stop the Spark context
-        sc.stop();
-    }
+    // Stop the Spark context
+    sc.stop();
+  }
 }
